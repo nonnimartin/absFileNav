c528c4c7be02ac6390c196b8c381a9953ceebced
jon@lab5.co <jon@lab5.co>
2019-05-28 22:56:19 -0700

    First uploading working

--- absFileNav/upload/views.py
+++ absFileNav/upload/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 from .models import uploadFile
-
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
 
+    if request.method == 'POST' and request.FILES['myFile']:
+
+        print ('This file = ' + str(request.FILES['myFile']))
+        print ('This file = ' + str(request))
+
+        myfile = request.FILES['myFile']
+
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'upload/index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+
     template = loader.get_template('upload/index.html')
     context = dict()
+
+
     return HttpResponse(template.render(context, request))
