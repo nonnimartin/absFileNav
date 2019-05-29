873a34a88e070377a117184e8e685bb0effe9339
jon@lab5.co <jon@lab5.co>
2019-05-29 14:52:54 -0700

    Added filesystem tree creation and sending to view for visualization/selection

--- absFileNav/upload/views.py
+++ absFileNav/upload/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.template import loader
 from .models import uploadFile
 from django.core.files.storage import FileSystemStorage
-
+from django.conf import settings
+import hashlib
+from functools import partial
+from util import createTree
 
 def index(request):
 
@@ -14,22 +17,52 @@ def index(request):
 
         myfile   = request.FILES['myFile']
 
-        fs       = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
+        #if file is too big chunked will be true, and must be processed in stream
+        chunked  = request.FILES['myFile'].multiple_chunks()
+        print ('chunked = ' + str(chunked))
 
-        # store uploaded file data in db
-        upfile = uploadFile()
-        upfile.name = filename
+        if chunked:
+            pass
 
-        # add path by if it's default or chosen file destination
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(clean_file_name(myfile.name), myfile)
+            uploaded_file_url = fs.url(filename)
 
-        # save uploaded file
-        upfile.save()
+            # store uploaded file data in db
+            upfile = uploadFile()
+            upfile.name = clean_file_name(filename)
+            upfile.path = settings.MEDIA_ROOT
+
+            upfile.checksum = hash_file(myfile.open())
+
+            # add path by if it's default or chosen file destination
+
+            # save uploaded file
+            upfile.save()
 
     #page template and view variables
     template = loader.get_template('upload/index.html')
+
+    #get json of file system for saving and set in view
+    json_file_tree = createTree.path_hierarchy('.')
+
     context = dict()
+    context['json_file_tree'] = createTree.path_hierarchy('.')
 
 
     return HttpResponse(template.render(context, request))
+
+def replace_spaces(thisString):
+    return thisString.replace(' ', '_')
+
+def clean_file_name(fileName):
+    thisString = replace_spaces(fileName)
+    return thisString
+
+def hash_file(file, block_size=65536):
+    hasher = hashlib.md5()
+    for buf in iter(partial(file.read, block_size), b''):
+        hasher.update(buf)
+
+    return hasher.hexdigest()
---
+++ absFileNav/util/createTree.py
@@ -0,0 +1,30 @@
+import os
+import json
+import errno
+
+def path_hierarchy(path):
+    hierarchy = {
+        'type': 'folder',
+        'name': os.path.basename(path),
+        'path': path,
+    }
+
+    try:
+        hierarchy['children'] = [
+            path_hierarchy(os.path.join(path, contents))
+            for contents in os.listdir(path)
+        ]
+    except OSError as e:
+        if e.errno != errno.ENOTDIR:
+            raise
+        hierarchy['type'] = 'file'
+
+    return hierarchy
+
+
+def main():
+    print (json.dumps(path_hierarchy('.')))
+    return json.dumps(path_hierarchy('.'))
+
+if __name__ == '__main__':
+    main()
